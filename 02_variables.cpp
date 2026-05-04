fa16833acf4246c38e28335ab3974127290b5fa1
Nimbu Paani <rajan085910@gmail.com>
2026-05-04 20:48:30 +0530

    Update

--- 02_variables.cpp
+++ 02_variables.cpp
@@ -2,6 +2,8 @@
 
 int main()
 {
+    //integer
+
     int x = 5;
     int y = 6;
     int sum = x+y;
@@ -10,11 +12,30 @@ int main()
     std::cout << y << '\n';
     std::cout << sum << '\n';
 
+    //float or double
+
     double marks = 7.8;
     std::cout << marks << '\n';
 
+    //single chracter
+
     char grade = 'B';
     std::cout << grade << '\n';
+
+    //boolen (True or False)
+
+    bool student = true;
+    bool forsale = false;
+
+    std::cout << forsale << '\n';
+    std::cout << student << '\n';
+
+    //strings
+
+    std::string name = "Nimbu";
+
+    std::cout << "Hello " << name << '\n';
+
 
     return 0;
 }
