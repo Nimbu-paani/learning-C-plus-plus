533a98fa73ecf22e3ba416ac72e661e2ffe946a9
Nimbu Paani <rajan085910@gmail.com>
2026-05-04 21:00:54 +0530

    New Commit

---
+++ 03_constant.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    const double PI = 3.14;
+    
+    double radius = 10;
+    double cir = 2*PI*radius;
+
+    std::cout << cir << " cm" << '\n';
+
+    return 0;
+}
