48fed1094ec186df7b3283f6d21b8369a13295a0
Nimbu Paani <rajan085910@gmail.com>
2026-05-07 21:20:41 +0530

    Hypotenuse calculator of triangle

---
+++ 11_practice_set.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+    using namespace std;
+
+    double a,b,c;
+
+    cout << "Enter side A:";
+    cin >> a;
+
+    cout << "Enter side B:";
+    cin >> b;
+
+    a = pow(a , 2);
+    b = pow(b , 2);
+    c = sqrt(a + b);
+
+    cout << "Side C:" << c << '\n';
+
+
+    return 0;
+}
