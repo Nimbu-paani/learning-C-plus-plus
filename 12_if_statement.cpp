de4cb50ecb1754aa3697138ba370956c30919a88
Nimbu Paani <rajan085910@gmail.com>
2026-05-10 21:33:32 +0530

    if statement

---
+++ 12_if_statement.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+
+    int age;
+
+    cout << "Enter your age:";
+    cin >> age;
+
+    if (age >= 18)
+    {
+        cout << "You are an adult!" << '\n';
+    }
+    
+
+    return 0;
+}
