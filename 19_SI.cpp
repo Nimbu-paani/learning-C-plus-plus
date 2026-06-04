094e583e1dafd359d9126191f1eff6e1123d8eb5
Nimbu Paani <rajan085910@gmail.com>
2026-06-04 21:29:29 +0530

    new commit

---
+++ 19_SI.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int si, p, t, r;
+
+    cout << "enter principle: ";
+    cin >> p;
+
+    cout << "enter time(in years): ";
+    cin >> t;
+
+    cout << "enter rate: ";
+    cin >> r;
+
+    si = p * t * r / 100;
+
+    cout << "simple intrest: " << si << endl;
+
+    return 0;
+}
