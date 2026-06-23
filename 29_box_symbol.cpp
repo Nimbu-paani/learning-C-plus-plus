1815880bdab27ceeedd22c2bf450857824600fdd
Nimbu Paani <rajan085910@gmail.com>
2026-06-23 20:36:35 +0530

    new code

---
+++ 29_box_symbol.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int rows, columns;
+    char sym;
+
+    cout << "Enter the number of rows:";
+    cin >> rows;
+    cout << "Enter the number of columns:";
+    cin >> columns;
+
+    cout << "Which symbol you want:";
+    cin >> sym;
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            cout << sym;
+        }
+        cout << '\n';
+    }
+
+    return 0;
+}
