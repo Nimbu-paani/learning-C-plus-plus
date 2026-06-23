429f6d32bf0bbf9a885423146294925ee4142f18
Nimbu Paani <rajan085910@gmail.com>
2026-06-23 21:03:09 +0530

    made a better dice roll

--- 30_diceroll.cpp
+++ 30_diceroll.cpp
@@ -4,13 +4,23 @@ using namespace std;
 
 int main()
 {
-    int face;
+    int face, again, num;
+    again = 1;
     cout << "How many numbers your dice have:";
     cin >> face;
 
-    srand(time(NULL));
-    int num = (rand() % face) + 1;
+    do
+    {
+        srand(time(NULL));
+        num = (rand() % face) + 1;
+        cout << "--------------------------------------\n";
+        cout << "Your rolled a " << num << "!" << '\n';
+        cout << "--------------------------------------\n";
+        cout << "1.Roll again\n2.exit\n";
+        cout << "Input:";
+        cin >> again;
 
-    cout << "Your rolled a " << num << "!" << '\n';
+    } while (again == 1);
+
     return 0;
 }
