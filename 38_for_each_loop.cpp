6502aebc39e5afd14349b9ee3f174a0331c37fa3
Nimbu Paani <rajan085910@gmail.com>
2026-07-04 22:00:41 +0530

    for each loop added

---
+++ 38_for_each_loop.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[] ={10,20,50,80,100,25};
+
+    for (int arr : arr)
+    {
+        cout << arr << endl;
+    }
+    
+    return 0;
+}
