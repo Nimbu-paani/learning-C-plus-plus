4a7ddd932e9bac7543a2696716cad99f75526025
Nimbu Paani <rajan085910@gmail.com>
2026-07-04 21:54:07 +0530

    fixed code

--- 37_size_of.cpp
+++ 37_size_of.cpp
@@ -4,11 +4,17 @@ using namespace std;
 int main()
 {
     int count;
-    string name[] = {"Nimbu" , "Lemon" , "Rajan"};
+    string name[] = {"Nimbu" , "Lemon" , "Rajan" , "Ayush"};
     
     count = sizeof(name)/sizeof(name[0]);
 
     cout << count << " Students\n";
+
+    for (long unsigned int i = 0; i < sizeof(name)/sizeof(name[0]); i++)
+    {
+        cout << name[i] << endl;
+    }
+    
 
     return 0;
 }
