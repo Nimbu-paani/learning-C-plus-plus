91701c17d545b73096c6a502ff3144e767ec3db2
Nimbu Paani <rajan085910@gmail.com>
2026-07-06 21:01:23 +0530

    new

---
+++ 41_address.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string name = "Nimbu";
+    int age =24;
+    bool student = true;
+
+    cout << &name << endl;
+    cout << &age << endl;
+    cout << &student << endl;
+    return 0;
+}
