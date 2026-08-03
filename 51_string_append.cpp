b5f88c46a9f14bcdc1698556047144cfb8b8fc5c
Nimbu Paani <rajan085910@gmail.com>
2026-08-03 21:42:18 +0530

    append

---
+++ 51_string_append.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string first = "Rajan";
+    string last = "Singh";
+
+    cout << first << ' ' << last << endl;
+    cout << first + ' ' + last << endl;
+
+    return 0;
+}
