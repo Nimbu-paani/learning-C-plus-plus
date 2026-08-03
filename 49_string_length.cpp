1ec2db5735443447c12e3fcee0ce0999046c5d09
Nimbu Paani <rajan085910@gmail.com>
2026-08-03 21:39:03 +0530

    string

---
+++ 49_string_length.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+
+int main ()
+{
+  std::string name ("Test string");
+  std::cout << "The size of str is " << name.length() << " bytes.\n";
+  return 0;
+}
---
+++ 50_c-style_string.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char name[] = "Nimbu Paani";
+
+    cout << name << endl;
+    return 0;
+}
