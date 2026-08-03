4f401272337e0bef6277477a06a8bc914dd1fd75
Nimbu Paani <rajan085910@gmail.com>
2026-08-03 21:44:41 +0530

    compare

---
+++ 52_string_compare.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string a = "apple";
+    string b = "fruit";
+
+    if (a==b)
+    {
+        cout << "same" << '\n';
+    }
+    else
+    cout << "not same" << '\n';
+
+    return 0;
+}
