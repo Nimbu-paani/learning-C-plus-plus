935f564f542c545de7e96d7a4317b939dcc27c5b
Nimbu Paani <rajan085910@gmail.com>
2026-08-10 21:27:56 +0530

    dynamic memory

---
+++ 56_dynamic_memory.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int *pNum = NULL;
+
+    pNum=new int;
+
+    *pNum = 21;
+
+    cout << "Address: " << pNum << '\n';
+    cout << "Value: " << *pNum << '\n';
+
+    delete pNum;
+    
+    return 0;
+}
