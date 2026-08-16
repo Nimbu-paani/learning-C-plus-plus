0f6f0cf33e6ac4b887c3f5d992580351ca49b2c9
Nimbu Paani <rajan085910@gmail.com>
2026-08-16 20:48:22 +0530

    few changes

--- 64_constructor.cpp
+++ 64_constructor.cpp
@@ -17,9 +17,19 @@ class student{
 int main()
 {
     student first("Rajan" , 24 , 75.8);
+    student second("Nimbu" , 24 , 64.8);
 
+    std::cout << "************" << std::endl;
     std::cout << "Name: " << first.name << '\n';
     std::cout << "Age: " << first.age << '\n';
     std::cout << "Marks: " << first.marks << '\n';
+
+    std::cout << "************" << std::endl;
+
+    std::cout << "Name: " << second.name << '\n';
+    std::cout << "Age: " << second.age << '\n';
+    std::cout << "Marks: " << second.marks << '\n';
+    std::cout << "************" << std::endl;
+
     return 0;
 }
